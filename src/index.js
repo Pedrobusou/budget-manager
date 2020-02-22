d8f96e8e0dc48e94a314e7e31ae6b951a3a34cb1
Pedro <pedrobusou@gmail.com>
2020-02-22 17:58:58 +0100

    Cleanup & add styles and CDNs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -10,3 +10,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+module.hot && module.hot.accept();
