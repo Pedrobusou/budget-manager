6a0769a8b26f3625715997669b95a3eb55c22453
Pedro <pedrobusou@gmail.com>
2020-02-22 23:22:56 +0100

    Conditional remaining class

--- src/components/RemainingBudget.js
+++ src/components/RemainingBudget.js
@@ -1,10 +1,15 @@
 import React, {Fragment} from 'react';
+import {getRemainingClass} from '../helpers';
 
-const RemainingBudget = ({budget, remaining}) => (
-  <Fragment>
-    <div className="alert alert--primary">Budget: {budget} €</div>
-    <div className="alert">Remaining: {remaining} €</div>
-  </Fragment>
-);
+const RemainingBudget = ({budget, remaining}) => {
+  return (
+    <Fragment>
+      <div className="alert alert--primary">Budget: {budget} €</div>
+      <div className={getRemainingClass(budget, remaining)}>
+        Remaining: {remaining} €
+      </div>
+    </Fragment>
+  );
+};
 
 export default RemainingBudget;
