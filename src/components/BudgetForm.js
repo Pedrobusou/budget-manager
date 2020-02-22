24df1bff01508af225636fd89ffa6dd8ee4ba64c
Pedro <pedrobusou@gmail.com>
2020-02-22 18:29:54 +0100

    Hard file rename 2

---
+++ src/components/BudgetForm.js
@@ -0,0 +1,46 @@
+import React, {Fragment, useState} from 'react';
+
+const BudgetForm = ({}) => {
+  const [budget, setBudget] = useState(0);
+  const [error, setError] = useState(false);
+
+  const handleChange = event => {
+    setBudget(+event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    setError(false);
+
+    if (budget < 1) {
+      setError(true);
+      return;
+    }
+
+    setError(false);
+
+    //TODO: next actions
+  };
+
+  return (
+    <Fragment>
+      <h2>Set up your budget</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          className="u-full-width"
+          placeholder="Set up your budget"
+          onChange={handleChange}
+        />
+        <input
+          type="submit"
+          className="button-primary u-full-width"
+          value="Set"
+        />
+      </form>
+    </Fragment>
+  );
+};
+
+export default BudgetForm;
