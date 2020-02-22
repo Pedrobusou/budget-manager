15643c69ddcdfcfe8abb427a3e47aa7824de920e
Pedro <pedrobusou@gmail.com>
2020-02-22 18:16:38 +0100

    Set up event handlers

--- src/components/budgetForm.js
+++ src/components/budgetForm.js
@@ -1,14 +1,28 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
 const BudgetForm = ({}) => {
+  const [budget, setBudget] = useState(0);
+
+  const handleChange = event => {
+    setBudget(+event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    //TODO: validate
+    //TODO: next actions
+  };
+
   return (
     <Fragment>
       <h2>Set up your budget</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="number"
           className="u-full-width"
           placeholder="Set up your budget"
+          onChange={handleChange}
         />
         <input
           type="submit"
